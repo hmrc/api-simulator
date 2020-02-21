bf4d89b990bc10a96f3bf3386fe3450d91a2d435
Billy Mitchell <26899143+mitchellb1@users.noreply.github.com>
2020-02-21 09:45:57 +0000

    Fix version issues

--- test/it/uk/gov/hmrc/apisimulator/PlatformIntegrationSpec.scala
+++ test/it/uk/gov/hmrc/apisimulator/PlatformIntegrationSpec.scala
@@ -17,12 +17,14 @@
 package it.uk.gov.hmrc.apisimulator
 
 import akka.stream.Materializer
+import controllers.AssetsMetadata
 import org.scalatest.TestData
 import org.scalatest.concurrent.ScalaFutures
 import org.scalatestplus.play.OneAppPerTest
+import org.scalatestplus.play.guice.GuiceOneAppPerSuite
 import play.api.http.LazyHttpErrorHandler
 import play.api.inject.guice.GuiceApplicationBuilder
-import play.api.test.FakeRequest
+import play.api.test.{FakeRequest, StubControllerComponentsFactory}
 import play.api.{Application, Mode}
 import play.mvc.Http.Status.OK
 import uk.gov.hmrc.apisimulator.controllers.Documentation
@@ -37,7 +39,7 @@ import uk.gov.hmrc.play.test.UnitSpec
   *
   * See: "API Platform Architecture with Flows" on Confluence.
   */
-class PlatformIntegrationSpec extends UnitSpec with ScalaFutures with OneAppPerTest {
+class PlatformIntegrationSpec extends UnitSpec with ScalaFutures with OneAppPerTest with StubControllerComponentsFactory {
 
   override def newAppForTest(testData: TestData): Application = GuiceApplicationBuilder()
     .configure("run.mode" -> "Stub")
@@ -48,7 +50,8 @@ class PlatformIntegrationSpec extends UnitSpec with ScalaFutures with OneAppPerT
 
   trait Setup {
     implicit val mat: Materializer = app.materializer
-    val documentationController = new Documentation(LazyHttpErrorHandler) {}
+    val meta = app.injector.instanceOf[AssetsMetadata]
+    val documentationController = new Documentation(LazyHttpErrorHandler, stubControllerComponents(), meta) {}
     val request = FakeRequest()
   }
 
